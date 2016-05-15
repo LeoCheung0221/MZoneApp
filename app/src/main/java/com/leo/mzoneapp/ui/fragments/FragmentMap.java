586b2e586112991a594af41454c6da1665cb2a20
LeoCheung0221 <LeoCheung0221>
2016-05-15 23:12:37 +0800

    add map stub

--- app/src/main/java/com/leo/mzoneapp/ui/fragments/FragmentMap.java
+++ app/src/main/java/com/leo/mzoneapp/ui/fragments/FragmentMap.java
@@ -5,20 +5,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.leo.mzoneapp.R;
 
 /**
  * author： LeoCheung4ever on 2016/5/9 16:21
  * email： leocheung4ever@gmail.com
- * description: TODO
+ * description: Google Map
  * what & why is modified:
  */
-public class FragmentMap extends FragmentBase {
+public class FragmentMap extends FragmentBase
+        implements OnMapReadyCallback {
+
+    GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_map,container,false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
